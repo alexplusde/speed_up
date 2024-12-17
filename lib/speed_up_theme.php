6fc734deb4739b39cc759e187eadea37bd065a50
alxndr-w <alxndr-w@users.noreply.github.com>
2024-12-17 14:34:05 +0000

    Apply php-cs-fixer changes

--- lib/speed_up_theme.php
+++ lib/speed_up_theme.php
@@ -5,7 +5,7 @@ class speed_up_theme
     public static function getUrl($file, $show_timestamp = true)
     {
         if ($show_timestamp) {
-            $timestamp = '?timestamp=' . @filemtime(rex_path::base('theme/'.$file));
+            $timestamp = '?timestamp=' . @filemtime(rex_path::base('theme/' . $file));
         }
 
         return rex_url::assets($file) . $timestamp;
